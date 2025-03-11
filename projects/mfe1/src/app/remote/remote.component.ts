229336862e6d75ffdb225f8970e1b0f985c26f17
Turbo Xav <xavier.tagliarino@gmail.com>
2025-03-11 11:23:01 +0100

    feat(mf): Add remote component

---
+++ projects/mfe1/src/app/remote/remote.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remote',
+  imports: [],
+  templateUrl: './remote.component.html',
+  styleUrl: './remote.component.scss'
+})
+export class RemoteComponent {
+
+}
